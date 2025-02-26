a8d9a74c4e9f0b69a2dcd5723355948a2ad469c1
jothsnapraveena <jothsnapraveena1421@gmail.com>
2025-02-26 04:52:26 -0500

    'added_data'

--- test.py
+++ test.py
@@ -6,9 +6,12 @@ Data=[
     {"name": "Jothsna","age":30,"city":"Boston"},
     {"name": "Mohan","age":35,"city":"NewYork"},
     {"name": "Trishika","age":4,"city":"Virginia"},
-    {"name": "Ravi","age":35,"city":"Niagara"},
+    {"name": "Ravi","age":35,"city":"Niagara"}
 ]
 
-pd.DataFrame(Data)
+df=pd.DataFrame(Data)
 
-Data.to_csv("data/data.csv",index=False)
+df.to_csv("data/data.csv",index=False)
+
+
+print("CSV file saved successfully!")
