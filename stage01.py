557cd8ffeb629124e8e66253ecc670d5a5b1be65
jothsnapraveena <jothsnapraveena1421@gmail.com>
2025-03-06 11:51:20 -0500

    'dvc_repro'

---
+++ stage01.py
@@ -0,0 +1,6 @@
+with open("artifact.txt","w") as f:
+    f.write("my name is jothsna and here I am executing stage 1")
+
+with open("artifact2.txt","w") as f:
+    f.write("I am learning MLOPS")
+    
